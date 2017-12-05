174687d61d5d41e0d23ec90bfe043eaf2bc163ff
Kristin Beno <kristinbeno@gmail.com>
2017-12-05 08:08:00 -0500

    tweaks

--- cookie.js
+++ cookie.js
@@ -1,18 +1,36 @@
 (function playCookieClicker() {
   setTimeout(function() { if (Game.cookiesEarned < 1000000) { Game.Reset(); window.alert('You ran out of time before reaching 1 Million cookies :('); }}, 1 * 60 * 1000);
   
-  // all of your code goes here
   
-var cookies = document.getElementById('cookies');
-var bigCookie = document.getElementById('bigCookie');
+  //Clickable cookie
+  var bigCookie = document.getElementById('bigCookie');
 
 
-// When cookie is clicked, change inner html to 1 cookie per second
-function updateCookieCount() {
-	cookies.innerHTML = '10 cookies per second';
-};
-bigCookie.addEventListener('click', updateCookieCount, false);
- 
+// Change inner html of cookie to '10 cookie per second'
+	function updateCookieCount() {
+		var cookies = document.getElementById('cookies');
+		cookies.innerHTML = '10 cookies per second';
+	};
+
+	
+// If cookie is clicked
+	bigCookie.addEventListener('click', updateCookieCount, false);
  
  
 })();
+
+/*
+
+	var cookieNumbers = document.getElementById('cookieNumbers');
+	cookieNumbers = 5;
+
+
+
+	bigCookie.addEventListener('click', showContents, false); 
+ 
+ 
+	function showContents() {
+		console.log(cookieNumbers.innerHTML);
+	}
+
+*/
