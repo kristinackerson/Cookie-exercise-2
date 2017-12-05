38393f1b3dec18abc61d5ca99aac7ee79a325f2f
Kristin Beno <kristinbeno@gmail.com>
2017-12-04 23:51:08 -0500

    Test

---
+++ cookie.js
@@ -0,0 +1,21 @@
+(function playCookieClicker() {
+  setTimeout(function() { if (Game.cookiesEarned < 1000000) { Game.Reset(); window.alert('You ran out of time before reaching 1 Million cookies :('); }}, 1 * 60 * 1000);
+  
+  // all of your code goes here
+  
+var cookies = document.getElementById('cookies');
+var bigCookie = document.getElementById('bigCookie');
+
+
+// When cookie is clicked, change inner html to 1 cookie per second
+function updateCookieCount() {
+	cookies.length = '10 cookie per second';
+};
+bigCookie.addEventListener('click', updateCookieCount, false);
+ 
+ 
+ // 
+ 
+ 
+ 
+})();
